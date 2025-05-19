9a6e6ffe202d987265eae4ac32d7735165554a49
Enrico Arapi <enricoarapi@icloud.com>
2025-05-19 14:36:26 +0100

    Initial commit

---
+++ SDP Study 1/VolumeView.swift
@@ -0,0 +1,18 @@
+//
+//  VolumeView.swift
+//  SDP Study 1
+//
+//  Created by Enrico Arapi on 02/04/2025.
+//
+
+import SwiftUI
+
+struct VolumeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VolumeView()
+}
